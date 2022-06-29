f16d3382605ffba553a3694bf85ab6b026b4b7fa
Abu-safyan yusuf <101343314+Sufyan-codes@users.noreply.github.com>
2022-06-29 23:56:39 +0100

    Update 102-magic_calculaion.py

--- 0x01-python-if_else_loops_functions/102-magic_calculaion.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculaion.py
@@ -1,8 +1,7 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def magic_calculaion(a, b, c):
     if a < b:
         return (c)
     if c > b:
         return (a + b)
     return (a*b - c)
-    git update-index --chmod=+x
