9c144318cc4eff46e65389e41210d25c2e19c1e3
Abu-safyan yusuf <101343314+Sufyan-codes@users.noreply.github.com>
2022-06-29 23:28:08 +0100

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 islower = __import__('7-islower').islower
 def islower(c):
-    if ord(c) >= 97 and ord(c) <=122:
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
     else:
-            return False
+        return False
