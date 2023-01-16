9f25ab42c4dc989f44b54cc039a414a14b5a4a08
Abu-safyan yusuf <101343314+Sufyan-codes@users.noreply.github.com>
2023-01-16 12:12:50 +0100

    Update 3-starwars_title.js
    Update

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,14 +1,15 @@
 #!/usr/bin/node
+// Prints the title of a Star Wars movie where the episode number
+// matches the given integer
+
 const request = require('request');
+const argv = process.argv;
 let url = 'http://swapi.co/api/films/';
-let episode = process.argv[2];
-request(url + episode, function (err, response, body) {
+
+request(url + argv[2], function (err, res, body) {
   if (err) {
     console.log(err);
-  } else if (response.statusCode === 200) {
-    let jsonobj = JSON.parse(body);
-    console.log(jsonobj.title);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
   }
+  let json = JSON.parse(body);
+  console.log(json.title);
 });
