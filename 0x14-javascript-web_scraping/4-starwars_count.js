4c82796d5b3c18f8280eb4939686b8fd7ce2c52f
Abu-safyan yusuf <101343314+Sufyan-codes@users.noreply.github.com>
2023-01-16 12:18:25 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,22 +1,21 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-request(url, function (err, response, body) {
+
+request(url, function (err, data, body) {
   if (err) {
     console.log(err);
-  } else if (response.statusCode === 200) {
-    let films = JSON.parse(body).results;
-    let count = 0;
-    for (let filmIndex in films) {
-      let filmChars = films[filmIndex].characters;
-      for (let charIndex in filmChars) {
-        if (filmChars[charIndex].includes('18')) {
-          count++;
+  } else {
+    let counter = 0;
+    const films = JSON.parse(body).results;
+    for (let result = 0; result < films.length; result++) {
+      const characters = films[result].characters;
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j] === 'https://swapi-api.hbtn.io/api/people/18/' || characters[j] === 'http://swapi-api.hbtn.io/api/people/18/') {
+          counter += 1;
         }
       }
     }
-    console.log(count);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
+    console.log(counter);
   }
 });
